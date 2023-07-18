26387086028a897b8d7a7487c84d7383318eeb3d
Eben Quenneville <student.equenneville@gmail.com>
2023-07-18 11:57:18 -0400

    First steps in cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,20 @@
 
 int main()
 {
-  std::cout << "Hello, world!" << std::endl;
+  std::cout << "Eben Quenneville" << std::endl;
+  std::cout << "Classroom 24-115" << std::endl;
 
   int x = 5;
   int y = 7;
-  std::cout << "The sum of " << x << " and " << y << " is " << x + y << std::endl;
+
+  int z = x + y;
+  std::cout << "The sum of " << x << " and " << y << " is " << z << std::endl;
+
+  int w = x * y * z;
+  std::cout << "The product of " << x << " and " << y << " and " << z << " is " << w << std::endl;
+
+  int v = x / y;
+  std::cout << "The division of " << x << " and " << y << " is " << v << std::endl;
 
   return 0;
 }
